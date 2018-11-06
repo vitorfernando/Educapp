78d8fe8616dfd6b1ab499c0af30cfb2c80db96e1
Thiago Borges <bgsthiago@gmail.com>
2018-11-06 20:07:11 -0200

    add chat

---
+++ src/pages/chat/chat.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase } from "angularfire2/database";
+import * as firebase from "firebase/app";
+
+@IonicPage()
+@Component({
+  selector: 'page-chat',
+  templateUrl: 'chat.html',
+})
+export class ChatPage {
+  messages: FirebaseListObservable<any[]>;
+  message: string;
+  parent: string;
+  name: string;
+  ra: any;
+  codProf: any;
+
+  constructor(private db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
+    this.ra = '';
+    this.codProf = '';
+
+    if (this.navParams.data.studentName){
+      this.parent = this.navParams.data.studentName.parent;
+      this.name = this.navParams.data.studentName.name;
+      this.ra = this.navParams.data.studentName.ra;
+    }
+
+    if (this.navParams.data.item){
+      this.codProf = this.navParams.data.item.professor;
+    }
+
+    if (this.navParams.data.itemStudent){
+      this.parent = this.navParams.data.professorName;
+      this.ra = this.navParams.data.itemStudent.$key;
+      this.codProf = this.navParams.data.professorID;
+    }
+    this.messages = db.list('message/' + this.codProf + '/' + this.ra);
+  }
+
+  enviar(){
+    var date = new Date();
+    let m = {
+      name: this.parent,
+      text: this.message,
+      hour: date.getHours() + ':' + date.getMinutes()
+    };
+    this.messages.push(m)
+      .then(() => {
+        this.message = '';
+      })
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ChatPage');
+  }
+
+}
